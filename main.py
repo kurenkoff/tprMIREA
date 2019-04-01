e6add82bc9702a92cab50a53c268c9195418f502
kurenkoff <ikurenkov.serafim@gmail.com>
2019-04-01 10:30:26 +0300

    переписано польностью. Работает

--- main.py
+++ main.py
@@ -1,79 +1,103 @@
-from math import sqrt, fabs
+import numpy as np
+from math import fabs
 
 
-def fun(x):
-    return x[0] ** 2 - x[0] * x[1] + 3 * x[1] ** 2 - x[0]
+class Simplex:
+    # n - размерность задачи минимизации
+    # m - длина ребра симплекса
+    # eps - точность поиска
+    def __init__(self, n, m, eps):
+        self.m = m
+        self.n = n
+        self.eps = eps
+        self.beta1 = m * (np.sqrt(n + 1) - 1) / (n * np.sqrt(n))
+        self.beta2 = m * (np.sqrt(n + 1) + n - 1) / (n * np.sqrt(n))
 
+    def minimize(self, fnc, zero_x, max_iterations=10):
+        table = []
+        zero_x.append(fnc(zero_x))
+        table.append(zero_x)
+        # инициализация таблицы
+        for i in range(self.n):
+            tmp_x = []
+            for j in range(self.n):
+                tmp_x.append(zero_x[j] + self.beta1 if i == j else zero_x[j] + self.beta2)
+            tmp_x.append(fnc(tmp_x))
+            table.append(tmp_x)
+        print(table)
+        xc = []  # костыль
+        i = 1
+        # Итерационный процесс
+        while self.check_end(xc, table) and i <= max_iterations:
+            im = self.max(table)
+            new_x = self.iter(table, im, fnc)
+            print("new_x = ", new_x)
+            table.pop(im)
+            table.append(new_x)
+            xc = self.new_xc(table, fnc)
+            print("table = ", table)
+            i += 1
+        # возврат минимального значения
+        return self.min(table)
 
-def simplex_search(n, m, eps, x, func):
-    # initial process
-    beta1 = ((sqrt(n + 1) - 1) / (n * sqrt(2))) * m
-    beta2 = ((sqrt(n + 1) + n - 1) / (n * sqrt(2))) * m
-    vect_x = list()
-    vect_x.append(x)
-    f_val = list()
-    f_val.append(fun(x))
-    for i in range(n):
-        tmp_list = list()
-        for j in range(n):
+    # вовращает номер строки с максимальным значением оптимизируемой функции
+    def max(self, table):
+        im = 0
+        for i in range(self.n):
+            if table[i][self.n] > table[im][self.n]:
+                im = i
+        return im
+
+    def min(self, table):
+        im = 0
+        for i in range(self.n):
+            if table[i][self.n] < table[im][self.n]:
+                im = i
+        return table[im][self.n]
+
+    # вычисление новой вершины симплекса
+    def iter(self, table, im, fnc):
+        xc = []
+        for i in range(self.n):
             tmp_x = 0
-            if i == j:
-                tmp_x = x[j] + beta1
-            else:
-                tmp_x = x[j] + beta2
-            tmp_list.append(tmp_x)
-        vect_x.append(tmp_list)
-        f_val.append(fun(tmp_list))
-    # iteration process
-    return iter(n, m, vect_x, f_val, eps)
+            for j in range(len(table)):
+                if j != im:
+                    tmp_x += table[j][i]
+            xc.append(tmp_x)
+        xc.append(0)
+        print("xc = ", str(xc))
+        tmp_x = []
+        for i in range(len(xc)):
+            tmp_x.append(xc[i] - table[im][i])
+        tmp_x.pop(self.n)
+        tmp_x.append(fnc(tmp_x))
+        return tmp_x
+
+    # проверка окончания вычисления
+    def check_end(self, xc, table):
+        if not xc:
+            return True
+        for i in range(self.n + 1):
+            if fabs(table[i][self.n] - xc[self.n]) > self.eps:
+                return True
+        return False
+
+    # вовращает значение центра тяжести текущего симплекса
+    def new_xc(self, table, fnc):
+        xc = []
+        for i in range(self.n):
+            tmp_x = 0
+            for j in range(self.n):
+                    tmp_x += table[j][i]
+            xc.append(tmp_x)
+        xc = (np.array(xc) * (1 / 3)).tolist()
+        xc.append(fnc(xc))
+        return xc
 
 
-def iter(n, m, vect_x, f_val, eps):
-    print(1)
-    mi = f_val.index(max(f_val))
-    fc = fxc(n, vect_x, mi)
-    # make new point
-    for i in range(n):
-        vect_x[mi][i] = fc[i] - vect_x[mi][i]
-    fx = f_val[mi]
-    f_val[mi] = fun(vect_x[mi])
-    if f_val[mi] >= fx:
-        # reduction
-        r = f_val.index(min(f_val))
-        for i in range(n + 1):
-            if i != r:
-                vect_x[i] = reduction(vect_x[i], vect_x[r])
-    xc = list()
-    for i in range(n):
-        tmp = 0
-        for j in range(n + 1):
-            tmp = vect_x[j][i]
-        xc.append(tmp)
-    f = fun(xc)
-    for i in range(n + 1):
-        if fabs(f_val[i] - f) >= eps:
-            return iter(n, m, vect_x, f_val, eps)
-    return vect_x[f_val.index(min(f_val))]
+def test_func(x):
+    return 1.2 * x[0] * x[0] - 2.4 * x[1] - 1.6
 
 
-def fxc(n, vect_x, mi):
-    fc = list()
-    for i in range(n):
-        tmp_x = 0
-        for j in range(n + 1):
-            if j != mi:
-                tmp_x += vect_x[j][i]
-        fc.append(tmp_x)
-    return fc
-
-
-def reduction(xi, xr):
-    result = list()
-    for i in range(len(xi)):
-        result.append(xr[i] + 0.5 * (xi[i] - xr[i]))
-    return result
-
-
-if __name__ == "__main__":
-    print(simplex_search(2, 0.25, 0.1, [0, 0], fun))
-
+s = Simplex(2, 0.25, 0.1)
+print(s.minimize(test_func, [-1, -1], max_iterations=5))
